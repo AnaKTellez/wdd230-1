ee9ce631ed3d8d596a25b896740bc1bbd4e55187
AnaKTellez <97267412+AnaKTellez@users.noreply.github.com>
2022-02-24 16:44:08 -0800

    formChanges

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ const year = date.getFullYear();
 
 document.querySelector("#year").textContent = year;
 document.getElementById("updated").innerHTML = `Last Updated: ${document.lastModified}`;
+
+const lastmod = document.querySelector("#lastmod");
+lastmod.innerHTML = `<em> Last Modification: ${document.lastModified}</em`;
